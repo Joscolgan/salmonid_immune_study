581f303a123d20128a0cce20a32115016b8f593f
Joe Colgan <tcolgan@tcd.ie>
2020-05-12 12:51:13 +0100

    Added script for creating second pass STAR indexes

---
+++ 05_variant_calling/run_star_index_second_pass.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+## Take inputs from the command line:
+input_fasta=$1
+input_gtf=$2
+overhang=$3
+
+## Check arguments are provided:
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Usage: ./make_star_index.sh input.fasta input.gtf overhang"
+    echo "For overhang, a value of the maximum read length minus 1 should be provided."
+
+fi
+
+## Create STAR indices
+STAR --runThreadN 1 \
+     --runMode genomeGenerate \
+     --genomeDir ./database/ \
+     --genomeFastaFiles "$input_fasta" \
+     --sjdbFileChrStartEnd /database/SJ.out.tab
+     --sjdbGTFfile "$input_gtf" \
+     --sjdbOverhang "$overhang"
