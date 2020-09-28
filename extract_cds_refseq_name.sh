61469ac60b339534e29f3cf23c9b9e5207a831a3
Joe Colgan <tcolgan@tcd.ie>
2020-09-28 12:32:20 +0100

    Update extract_cds_refseq_name.sh

--- extract_cds_refseq_name.sh
+++ extract_cds_refseq_name.sh
@@ -10,3 +10,15 @@ awk '$3=="CDS"' | head -n 1 | cut -f 9 | cut -d ';' -f 2 | \
 sed 's/Parent=rna-//g' >> "$new_name"_transcript_names.txt;
 done < "$name";
 done
+
+
+for name in *refseq.txt;
+do
+new_name="$(echo "$name" | cut -d '.' -f 1 )";
+echo "$new_name";
+while read line;
+do 
+grep "$line" ~/projects/2020-04-11_salmonid_immune_project/2020-04-11_orthofinder_of_immune_genes/data/2020-04-11_refseq_datasets/combined_cds_headers.txt | \
+sed 's/>//g' >> "$new_name"_new_transcript_names.txt;
+done < "$name";
+done
